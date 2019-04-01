6ae8ee348412bee7759039b62d58319c5a387cf7
hexueguo <791844144@qq.com>
2019-04-01 09:36:47 +0800

    update style

--- source/index.js
+++ source/index.js
@@ -1,329 +1,395 @@
-import * as helper from './helper'
-import React, { Component } from 'react'
+/* eslint-disable  */
+import React, { Component } from "react";
 import {
-    StyleSheet,
-    PanResponder,
-    View,
-    Text,
-    Dimensions,
-    I18nManager
-} from 'react-native'
-import Line from './line'
-import Circle from './circle'
-import PropTypes from 'prop-types';
+  StyleSheet,
+  PanResponder,
+  View,
+  Text,
+  Dimensions,
+  I18nManager,
+} from "react-native";
+import PropTypes from "prop-types";
+import Line from "./line";
+import * as helper from "./helper";
+import Circle from "./circle";
 
-const Width = Dimensions.get('window').width;
-const Height = Dimensions.get('window').height;
+const Width = Dimensions.get("window").width;
+const Height = Dimensions.get("window").height;
 const isVertical = Height > Width;
-const Top = isVertical ? (Height - Width)/2.0 * 1.25 : 10;
+const Top = isVertical ? ((Height - Width) / 2.0) * 1.25 : 10;
 const Radius = isVertical ? Width / 10 : Width / 25;
 
 export default class GesturePassword extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.timer = null;
-        this.lastIndex = -1;
-        this.sequence = '';   // 手势结果
-        this.isMoving = false;
+    this.timer = null;
+    this.lastIndex = -1;
+    this.sequence = ""; // 手势结果
+    this.isMoving = false;
 
-        // getInitialState
-        let circles = [];
-        let Margin = Radius;
-        for (let i=0; i < 9; i++) {
-            let p = i % 3;
-            let q = parseInt(i / 3);
-            circles.push({
-                isActive: false,
-                x: p * (Radius * 2 + Margin) + Margin + Radius,
-                y: q * (Radius * 2 + Margin) + Margin + Radius
-            });
-        }
-
-        this.state = {
-            circles: circles,
-            lines: []
-        }
+    // getInitialState
+    const circles = [];
+    const Margin = Radius;
+    for (let i = 0; i < 9; i++) {
+      const p = i % 3;
+      const q = parseInt(i / 3);
+      circles.push({
+        isActive: false,
+        x: p * (Radius * 2 + Margin) + Margin + Radius,
+        y: q * (Radius * 2 + Margin) + Margin + Radius,
+      });
     }
 
-    componentWillMount() {
-        this._panResponder = PanResponder.create({
-            // 要求成为响应者：
-            onStartShouldSetPanResponder: (event, gestureState) => true,
-            onStartShouldSetPanResponderCapture: (event, gestureState) => true,
-            onMoveShouldSetPanResponder: (event, gestureState) => true,
-            onMoveShouldSetPanResponderCapture: (event, gestureState) => true,
+    this.state = {
+      circles,
+      lines: [],
+      message: "",
+    };
+  }
 
-            // 开始手势操作
-            onPanResponderGrant: (event, gestureState) => {
-                this.onStart(event, gestureState);
-            },
-            // 移动操作
-            onPanResponderMove: (event, gestureState) => {
-                this.onMove(event, gestureState);
-            },
-            // 释放手势
-            onPanResponderRelease: (event, gestureState) => {
-                this.onEnd(event, gestureState);
-            }
-        })
+  componentWillMount() {
+    this._panResponder = PanResponder.create({
+      // 要求成为响应者：
+      onStartShouldSetPanResponder: (event, gestureState) => true,
+      onStartShouldSetPanResponderCapture: (event, gestureState) => true,
+      onMoveShouldSetPanResponder: (event, gestureState) => true,
+      onMoveShouldSetPanResponderCapture: (event, gestureState) => true,
+
+      // 开始手势操作
+      onPanResponderGrant: (event, gestureState) => {
+        this.onStart(event, gestureState);
+      },
+      // 移动操作
+      onPanResponderMove: (event, gestureState) => {
+        this.onMove(event, gestureState);
+      },
+      // 释放手势
+      onPanResponderRelease: (event, gestureState) => {
+        this.onEnd(event, gestureState);
+      },
+    });
+  }
+
+  onStart(e, g) {
+    let x = isVertical
+      ? e.nativeEvent.pageX
+      : e.nativeEvent.pageX - Width / 3.4;
+    let y = isVertical
+      ? e.nativeEvent.pageY - Top / 1.25
+      : e.nativeEvent.pageY - 30;
+
+    let lastChar = this.getTouchChar({ x, y });
+    if (lastChar) {
+      this.isMoving = true;
+      this.lastIndex = Number(lastChar);
+      this.sequence = lastChar;
+      this.resetActive();
+      this.setActive(this.lastIndex);
+
+      let point = {
+        x: this.state.circles[this.lastIndex].x,
+        y: this.state.circles[this.lastIndex].y,
+      };
+
+      this.refs.line.setNativeProps({ start: point, end: point });
+
+      this.props.onStart && this.props.onStart();
+
+      if (this.props.interval > 0) {
+        clearTimeout(this.timer);
+      }
     }
+  }
 
-    render() {
-        let color = this.props.status === 'wrong' ? this.props.wrongColor : this.props.rightColor;
+  onMove(e, g) {
+    let x = isVertical
+      ? e.nativeEvent.pageX
+      : e.nativeEvent.pageX - Width / 3.4;
+    let y = isVertical
+      ? e.nativeEvent.pageY - Top / 1.25
+      : e.nativeEvent.pageY - 30;
 
-        return (
-            <View style={[styles.frame, this.props.style, {flex: 1}]}>
-                <View style={styles.message}>
-                    <Text style={[styles.msgText, this.props.textStyle, {color: color}]}>
-                        {this.state.message || this.props.message}
-                    </Text>
-                </View>
-                <View style={styles.board} {...this._panResponder.panHandlers}>
-                    {this.renderCircles()}
-                    {this.renderLines()}
-                    <Line ref='line' color={ this.props.transparentLine ? '#00000000' : color} />
-                </View>
+    if (this.isMoving) {
+      this.refs.line.setNativeProps({ end: { x, y } });
 
-                {this.props.children}
-            </View>
+      let lastChar = null;
+
+      if (
+        !helper.isPointInCircle(
+          { x, y },
+          this.state.circles[this.lastIndex],
+          Radius
         )
-    }
+      ) {
+        lastChar = this.getTouchChar({ x, y });
+      }
 
-    renderCircles() {
-        let array = [], fill, color, inner, outer;
-        let { status, normalColor, wrongColor, rightColor, innerCircle, outerCircle } = this.props;
+      if (lastChar && this.sequence.indexOf(lastChar) === -1) {
+        if (!this.props.allowCross) {
+          let crossChar = this.getCrossChar(lastChar);
 
-        this.state.circles.forEach(function(c, i) {
-            fill = c.isActive;
-            color = status === 'wrong' ? wrongColor : rightColor;
-            inner = !!innerCircle;
-            outer = !!outerCircle;
+          if (crossChar && this.sequence.indexOf(crossChar) === -1) {
+            this.sequence += crossChar;
+            this.setActive(Number(crossChar));
+          }
+        }
 
-            array.push(
-                <Circle key={'c_' + i} fill={fill} normalColor={normalColor} color={color} x={c.x} y={c.y} r={Radius} inner={inner} outer={outer} />
-            )
+        let lastIndex = this.lastIndex;
+        let thisIndex = Number(lastChar);
+
+        this.state.lines.push({
+          start: {
+            x: this.state.circles[lastIndex].x,
+            y: this.state.circles[lastIndex].y,
+          },
+          end: {
+            x: this.state.circles[thisIndex].x,
+            y: this.state.circles[thisIndex].y,
+          },
         });
 
-        return array;
+        this.lastIndex = Number(lastChar);
+        this.sequence += lastChar;
+
+        this.setActive(this.lastIndex);
+
+        let point = {
+          x: this.state.circles[this.lastIndex].x,
+          y: this.state.circles[this.lastIndex].y,
+        };
+
+        this.refs.line.setNativeProps({ start: point });
+      }
     }
 
-    renderLines() {
-        let array = [], color;
-        let { status, wrongColor, rightColor, transparentLine } = this.props;
+    if (this.sequence.length === 9) this.onEnd();
+  }
 
-        this.state.lines.forEach(function(l, i) {
-            color = status === 'wrong' ? wrongColor : rightColor;
-            color = transparentLine ? '#00000000' : color;
+  onEnd(e, g) {
+    if (this.isMoving) {
+      let password = helper.getRealPassword(this.sequence);
+      this.sequence = "";
+      this.lastIndex = -1;
+      this.isMoving = false;
 
-            array.push(
-                <Line key={'l_' + i} color={color} start={l.start} end={l.end} />
-            )
-        });
+      let origin = { x: 0, y: 0 };
+      this.refs.line.setNativeProps({ start: origin, end: origin });
 
-        return array;
+      this.props.onEnd && this.props.onEnd(password);
+
+      if (this.props.interval > 0) {
+        this.timer = setTimeout(() => this.resetActive(), this.props.interval);
+      }
+    }
+  }
+
+  getCrossChar(char) {
+    let middles = "13457",
+      last = String(this.lastIndex);
+
+    if (middles.indexOf(char) > -1 || middles.indexOf(last) > -1) return false;
+
+    let point = helper.getMiddlePoint(
+      this.state.circles[last],
+      this.state.circles[char]
+    );
+
+    for (let i = 0; i < middles.length; i++) {
+      let index = middles[i];
+      if (helper.isEquals(point, this.state.circles[index])) {
+        return String(index);
+      }
     }
 
-    setActive(index) {
-        this.state.circles[index].isActive = true;
+    return false;
+  }
 
-        let circles = this.state.circles;
-        this.setState({circles});
+  getTouchChar(touch) {
+    let x = touch.x;
+    let y = touch.y;
+
+    for (let i = 0; i < 9; i++) {
+      if (helper.isPointInCircle({ x, y }, this.state.circles[i], Radius)) {
+        return String(i);
+      }
     }
 
-    resetActive() {
-        this.state.lines = [];
-        for (let i=0; i < 9; i++) {
-            this.state.circles[i].isActive = false;
-        }
+    return false;
+  }
 
-        let circles = this.state.circles;
-        this.setState({circles});
-        this.props.onReset && this.props.onReset();
+  setActive(index) {
+    this.state.circles[index].isActive = true;
+
+    let circles = this.state.circles;
+    this.setState({ circles });
+  }
+
+  resetActive() {
+    this.state.lines = [];
+    for (let i = 0; i < 9; i++) {
+      this.state.circles[i].isActive = false;
     }
 
-    getTouchChar(touch) {
-        let x = touch.x;
-        let y = touch.y;
+    let circles = this.state.circles;
+    this.setState({ circles });
+    this.props.onReset && this.props.onReset();
+  }
 
-        for (let i=0; i < 9; i++) {
-            if ( helper.isPointInCircle({x, y}, this.state.circles[i], Radius) ) {
-                return String(i);
-            }
-        }
+  renderLines() {
+    let array = [],
+      color;
+    let { status, wrongColor, rightColor, transparentLine } = this.props;
 
-        return false;
-    }
+    this.state.lines.forEach(function(l, i) {
+      color = status === "wrong" ? wrongColor : rightColor;
+      color = transparentLine ? "#00000000" : color;
 
-    getCrossChar(char) {
-        let middles = '13457', last = String(this.lastIndex);
+      array.push(
+        <Line key={"l_" + i} color={color} start={l.start} end={l.end} />
+      );
+    });
 
-        if ( middles.indexOf(char) > -1 || middles.indexOf(last) > -1 ) return false;
+    return array;
+  }
 
-        let point = helper.getMiddlePoint(this.state.circles[last], this.state.circles[char]);
+  renderCircles() {
+    let array = [],
+      fill,
+      color,
+      outsideColor,
+      inner,
+      outer;
 
-        for (let i=0; i < middles.length; i++) {
-            let index = middles[i];
-            if ( helper.isEquals(point, this.state.circles[index]) ) {
-                return String(index);
-            }
-        }
+    let {
+      status,
+      normalColor,
+      wrongColor,
+      rightColor,
+      innerCircle,
+      outerCircle,
+      wrongOutsideColor,
+      righOutsideColor,
+    } = this.props;
 
-        return false;
-    }
+    this.state.circles.forEach(function(c, i) {
+      fill = c.isActive;
+      color = status === "wrong" ? wrongColor : rightColor;
+      outsideColor = status === "wrong" ? wrongOutsideColor : righOutsideColor;
 
-    onStart(e, g) {
-        let x = isVertical ? e.nativeEvent.pageX : e.nativeEvent.pageX - Width/3.4;
-        let y = isVertical ? e.nativeEvent.pageY - Top/1.25 : e.nativeEvent.pageY - 30;
+      inner = !!innerCircle;
+      outer = !!outerCircle;
 
-        let lastChar = this.getTouchChar({x, y});
-        if ( lastChar ) {
-            this.isMoving = true;
-            this.lastIndex = Number(lastChar);
-            this.sequence = lastChar;
-            this.resetActive();
-            this.setActive(this.lastIndex);
+      array.push(
+        <Circle
+          key={"c_" + i}
+          fill={fill}
+          normalColor={normalColor}
+          color={color}
+          outsideColor={outsideColor}
+          x={c.x}
+          y={c.y}
+          r={Radius}
+          inner={inner}
+          outer={outer}
+        />
+      );
+    });
 
-            let point = {
-                x: this.state.circles[this.lastIndex].x,
-                y: this.state.circles[this.lastIndex].y
-            };
+    return array;
+  }
 
-            this.refs.line.setNativeProps({start: point, end: point});
+  render() {
+    const { message: messageState } = this.state;
+    const {
+      status,
+      wrongColor,
+      rightColor,
+      style,
+      textStyle,
+      message,
+      transparentLine,
+      children,
+    } = this.props;
 
-            this.props.onStart && this.props.onStart();
+    const color = status === "wrong" ? wrongColor : rightColor;
 
-            if ( this.props.interval>0 ) {
-                clearTimeout(this.timer);
-            }
-        }
-    }
+    return (
+      <View style={[styles.frame, style, { flex: 1 }]}>
+        <View style={styles.message}>
+          <Text style={[styles.msgText, textStyle, { color }]}>
+            {messageState || message}
+          </Text>
+        </View>
+        <View style={styles.board} {...this._panResponder.panHandlers}>
+          {this.renderCircles()}
+          {this.renderLines()}
+          <Line ref="line" color={transparentLine ? "#00000000" : color} />
+        </View>
 
-    onMove(e, g) {
-        let x = isVertical ? e.nativeEvent.pageX : e.nativeEvent.pageX - Width/3.4;
-        let y = isVertical ? e.nativeEvent.pageY - Top/1.25 : e.nativeEvent.pageY - 30;
-
-        if ( this.isMoving ) {
-            this.refs.line.setNativeProps({end: {x, y}});
-
-            let lastChar = null;
-
-            if ( !helper.isPointInCircle({x, y}, this.state.circles[this.lastIndex], Radius) ) {
-                lastChar = this.getTouchChar({x, y});
-            }
-
-            if ( lastChar && this.sequence.indexOf(lastChar) === -1 ) {
-                if ( !this.props.allowCross ) {
-                    let crossChar = this.getCrossChar(lastChar);
-
-                    if ( crossChar && this.sequence.indexOf(crossChar) === -1 ) {
-                        this.sequence += crossChar;
-                        this.setActive(Number(crossChar));
-                    }
-                }
-
-                let lastIndex = this.lastIndex;
-                let thisIndex = Number(lastChar);
-
-                this.state.lines.push({
-                    start: {
-                        x: this.state.circles[lastIndex].x,
-                        y: this.state.circles[lastIndex].y
-                    },
-                    end: {
-                        x: this.state.circles[thisIndex].x,
-                        y: this.state.circles[thisIndex].y
-                    }
-                });
-
-                this.lastIndex = Number(lastChar);
-                this.sequence += lastChar;
-
-                this.setActive(this.lastIndex);
-
-                let point = {
-                    x: this.state.circles[this.lastIndex].x,
-                    y: this.state.circles[this.lastIndex].y
-                };
-
-                this.refs.line.setNativeProps({start: point});
-            }
-        }
-
-        if ( this.sequence.length === 9 ) this.onEnd();
-    }
-
-    onEnd(e, g) {
-        if ( this.isMoving ) {
-            let password = helper.getRealPassword(this.sequence);
-            this.sequence = '';
-            this.lastIndex = -1;
-            this.isMoving = false;
-
-            let origin = {x: 0, y: 0};
-            this.refs.line.setNativeProps({start: origin, end: origin});
-
-            this.props.onEnd && this.props.onEnd(password);
-
-            if ( this.props.interval>0 ) {
-                this.timer = setTimeout(() => this.resetActive(), this.props.interval);
-            }
-        }
-    }
+        {children}
+      </View>
+    );
+  }
 }
 
 GesturePassword.propTypes = {
-    message: PropTypes.string,
-    normalColor: PropTypes.string,
-    rightColor: PropTypes.string,
-    wrongColor: PropTypes.string,
-    status: PropTypes.oneOf(['right', 'wrong', 'normal']),
-    onStart: PropTypes.func,
-    onEnd: PropTypes.func,
-    onReset: PropTypes.func,
-    interval: PropTypes.number,
-    allowCross: PropTypes.bool,
-    innerCircle: PropTypes.bool,
-    outerCircle: PropTypes.bool
+  message: PropTypes.string,
+  normalColor: PropTypes.string,
+  rightColor: PropTypes.string,
+  righOutsideColor: PropTypes.string,
+  wrongColor: PropTypes.string,
+  wrongOutsideColor: PropTypes.string,
+  status: PropTypes.oneOf(["right", "wrong", "normal"]),
+  onStart: PropTypes.func,
+  onEnd: PropTypes.func,
+  onReset: PropTypes.func,
+  interval: PropTypes.number,
+  allowCross: PropTypes.bool,
+  innerCircle: PropTypes.bool,
+  outerCircle: PropTypes.bool,
 };
 
 GesturePassword.defaultProps = {
-    message: '',
-    normalColor: '#5FA8FC',
-    rightColor: '#5FA8FC',
-    wrongColor: '#D93609',
-    status: 'normal',
-    interval: 0,
-    allowCross: false,
-    innerCircle: true,
-    outerCircle: true
+  message: "",
+  normalColor: "#007CFF",
+  righOutsideColor: "rgba(0,124,255,0.30)",
+  rightColor: "#007CFF",
+  righOutsideColor: "rgba(0,124,255,0.30)",
+  wrongColor: "#E8495A",
+  wrongOutsideColor: "#FBEFF1",
+  status: "normal",
+  interval: 0,
+  allowCross: false,
+  innerCircle: true,
+  outerCircle: true,
 };
 
 const styles = StyleSheet.create({
-    frame: {
-        flexDirection: I18nManager.isRTL ? 'row' : 'row-reverse',
-        backgroundColor: '#292B38'
-    },
-    board: {
-        flexDirection: I18nManager.isRTL ? 'row-reverse' : 'row',
-        position: 'absolute',
-        left: isVertical ? 0 : Width/3.4,
-        top: isVertical ? Top/1.5 : 30,
-        width: Width,
-        height: Height
-    },
-    message: {
-        position: 'absolute',
-        left: 0,
-        top: 20,
-        width: Width,
-        height: Top / 3,
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    msgText: {
-        fontSize: 14
-    }
+  frame: {
+    flexDirection: I18nManager.isRTL ? "row" : "row-reverse",
+    backgroundColor: "#292B38",
+  },
+  board: {
+    flexDirection: I18nManager.isRTL ? "row-reverse" : "row",
+    position: "absolute",
+    left: isVertical ? 0 : Width / 3.4,
+    top: isVertical ? Top / 1.5 : 30,
+    width: Width,
+    height: Height,
+  },
+  message: {
+    position: "absolute",
+    left: 0,
+    top: 40,
+    width: Width,
+    height: Top / 3,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  msgText: {
+    fontSize: 16,
+  },
 });
 
 module.exports = GesturePassword;
